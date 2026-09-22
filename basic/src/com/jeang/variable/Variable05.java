7668813f9f1c58a98e8b2679bf5d3e19e1f6000a
Jeang <2609231566@qq.com>
2026-09-22 23:00:33 +0800

    easy练习

---
+++ basic/src/com/jeang/variable/Variable05.java
@@ -0,0 +1,16 @@
+package com.jeang.variable;
+
+import java.util.Scanner;
+
+public class Variable05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        判断一个四位数是不是回文
+        System.out.println("输入一个四位数:");
+        int num = sc.nextInt();
+        if (num / 1000 == num % 10 && num / 100 % 10 == num / 10 % 10)
+            System.out.println(num + "是回文数");
+        else
+            System.out.println(num + "不是回文数");
+    }
+}

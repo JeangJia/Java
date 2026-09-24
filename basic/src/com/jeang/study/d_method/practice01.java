d9e0fbc7e19ddc3693fd31e423293752010e7d65
Jeang <2609231566@qq.com>
2026-09-24 23:28:22 +0800

    practice

--- basic/src/com/jeang/study/d_method/practice01.java
+++ basic/src/com/jeang/study/d_method/practice01.java
@@ -4,19 +4,20 @@ import java.util.Scanner;
 
 public class practice01 {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        double w=sc.nextDouble();
-        double price=0;
-        if(w<=1)price=10;
-        else if(w>1&&w<=5)price=10+one_five(w-1);
-        else price=10+one_five(4)+five_(w-5);
+        Scanner sc = new Scanner(System.in);
+        double w = sc.nextDouble();
+        double price = 0;
+        if (w <= 1) price = 10;
+        else if (w > 1 && w <= 5) price = 10 + one_five(w - 1);
+        else price = 10 + one_five(4) + five_(w - 5);
         System.out.println(price);
     }
-    public static double one_five(double w){
-        return w*2;
-    }
-    public static double five_(double w){
-        return w*1.5;
+
+    public static double one_five(double w) {
+        return w * 2;
     }
 
+    public static double five_(double w) {
+        return w * 1.5;
+    }
 }
